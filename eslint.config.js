5f2e3ac88768b5a3c328de6a1faf4d273cccf60e
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-04-13 20:10:59 +0400

    initial commot

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,17 @@ import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      "no-undef": "off",
+      "no-unused-vars": "warn",
+    },
+  },
 ]);
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env" });
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB)
+  .then(() => "Database connected successfully")
+  .catch(() => (error) => console.error(error.message, "DB conection failed!"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port:${port}`));
