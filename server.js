c87bf2f2e83ea0ccbe5a9b127e411dfa7916aed6
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-04-13 19:57:51 +0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port:${port}`));
