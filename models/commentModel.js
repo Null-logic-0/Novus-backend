b1096747d987de38b9c57f061a22104d85852afe
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-04-15 20:32:07 +0400

    feat:build comments

---
+++ models/commentModel.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      maxlength: 100,
+      minlength: 1,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    depth: {
+      type: Number,
+      default: 0,
+    },
+
+    parentComment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment", //  self-reference
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (_, ret) => {
+        delete ret.id;
+        return ret;
+      },
+    },
+    toObject: { virtuals: true },
+  }
+);
+
+commentSchema.virtual("replies", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "parentComment",
+});
+
+commentSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "user",
+    select: "fullName profileImage",
+  });
+  next();
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,10 +7,25 @@ import {
   getSinglePost,
   updatePost,
 } from "../controllers/PostControllers.js";
+import {
+  commentReply,
+  createComment,
+  deleteComment,
+  getPostComments,
+  updateComment,
+} from "../controllers/commentControllers.js";
 
 export const router = Router();
 
 router.use(protect);
 
+// Posts
 router.route("/").get(getAllPosts).post(createPost);
 router.route("/:id").get(getSinglePost).patch(updatePost).delete(deletePost);
+
+// Comments
+router.route("/:id/comments").get(getPostComments).post(createComment);
+
+// Comment reply nested route
+router.route("/:id/comments/:parentCommentId/replies").post(commentReply);
+router.route("/comments/:id").patch(updateComment).delete(deleteComment);
